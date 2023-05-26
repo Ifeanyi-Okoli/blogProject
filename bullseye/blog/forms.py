fdeb0a599179db2f4af606999be6675575ec8bc8
Okoli Ifeanyi <Okoli.I@bullseye.local>
2023-05-26 17:41:48 +0100

    comments added to the template files

--- bullseye/blog/forms.py
+++ bullseye/blog/forms.py
@@ -2,53 +2,54 @@ from django.shortcuts import render
 from .models import Post, Category, Comment
 from django import forms
 
-
-# choices = [('coding' 'coding'), ('sports' 'sports'), ('entertainment' 'entertainment')]
+# Get choices for category field from the Category model
 choices = Category.objects.all().values_list('name', 'name')
 
 choice_list = []
 
+# Create a list of choices for the category field
 for item in choices:
     choice_list.append(item)
 
+# Form for creating or updating a post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
         
+        # Set widget attributes for each form field
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title here'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title_tag here'}),
             'author': forms.Select(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
-            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter content here'}),
             'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter content here'}),
         }
         
 
+# Form for updating a post
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'title_tag', 'body',)
         
+        # Set widget attributes for each form field
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title here'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title_tag here'}),
-            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter content here'}),
-            # 'snippet': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter content here'}),
         }
         
         
+# Form for creating a comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'body')
         
+        # Set widget attributes for each form field
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title here'}),
-            
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter content here'}),
-            
         }
--- bullseye/blog/views.py
+++ bullseye/blog/views.py
@@ -1,18 +1,15 @@
 from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Category, Comment
+from .models import Post, Category, Comment  # Importing necessary models
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .forms import PostForm, EditForm, CommentForm
-from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView  # Importing generic views
+from .forms import PostForm, EditForm, CommentForm  # Importing necessary forms
+from django.urls import reverse_lazy, reverse  # Importing functions for URL handling
 
-#the Listwview pulls out all of the blog posts, while the Detail view pulls out a single blog post
-# Create your views here.
-
-# def home(request):
-#     return render(request, "home.html", {})
+# Define views here
 
 def LikeView(request, pk):
+    # View for handling post likes
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
     liked = False
     if post.likes.filter(id=request.user.id).exists():
@@ -23,57 +20,44 @@ def LikeView(request, pk):
         liked = True
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
-
 class HomeView(ListView):
+    # View for rendering the home page with a list of posts
     model = Post
     template_name = "home.html"
     cats = Category.objects.all()
     ordering = ['-post_date']
-    # ordering = ['-id']
     
     def get_context_data(self, *args, **kwargs):
+        # Get additional context data for the view
         cat_menu = Category.objects.all()
         context = super(HomeView, self).get_context_data(*args, **kwargs)
         context["cat_menu"] = cat_menu
         return context
 
-
 def CategoryListView(request):
+    # View for rendering the category list
     cat_menu_list = Category.objects.all()
     return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
-    
-    
-    
-    
-# def CategoryView(request, cats):
-#     category_posts = Post.objects.all().filter(category=cats)
-#     return render(request, 'categories.html', {'cats':cats, "category_posts":category_posts})
-    
-
 
 class CategoryView(ListView):
+    # View for rendering category-specific posts
     model = Post
     template_name = "categories.html"
     
     def get_context_data(self, *args, **kwargs):
+        # Get additional context data for the view
         category_posts = Post.objects.all()
         context = super(CategoryView, self).get_context_data(*args, **kwargs)
         context["category_posts"] = category_posts                
         return context
-    
-    # def get_context_data(self, **kwargs):
-    #     # category_posts = Post.objects.all()
-    #     context = super().get_context_data(**kwargs)
-    #     context["category_posts"] = Post.objects.all(category=self.kwargs['cats'])               
-    #     return context
- 
- 
-    
+
 class ArticleDetailView(DetailView):
+    # View for rendering a detailed view of a single post
     model = Post
     template_name = "article_details.html"
     
     def get_context_data(self, *args, **kwargs):
+        # Get additional context data for the view
         cat_menu = Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
         
@@ -88,30 +72,28 @@ class ArticleDetailView(DetailView):
         context['total_likes'] = total_likes
         context['liked'] = liked
         return context
-    
+
 class AddPostView(CreateView):
+    # View for adding a new post
     model = Post
     form_class = PostForm
     template_name = "add_post.html"
-    
-    # fields = ('title', 'body')
-    
- 
+
 class AddCommentView(CreateView):
+    # View for adding a new comment
     model = Comment
     form_class = CommentForm
     template_name = "add_comment.html" 
-    # fields = '__all__'
-    
     
     def form_valid(self, form):
+        # Set the post_id for the comment based on the URL parameter
         form.instance.post_id = self.kwargs['pk']
         return super().form_valid(form)
     
     success_url = reverse_lazy('Home')
- 
-   
+
 class AddCategoryView(CreateView):
+    # View for adding a new category
     model = Category
     # form_class = PostForm
     template_name = "add_category.html"
